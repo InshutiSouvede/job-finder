fd105cb9a314cfc85a6769f535f2ff33d0ac8bd4
InshutiSouvede <isouvede2@gmail.com>
2024-08-27 19:53:23 +0200

    Display all users on the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom'
+import { User } from './types'
+import { usersData } from './userData'
+
+
 export default function App() {
+  const users = usersData.map((user:User)=>(
+    <div key={user.id} className=" border justify-between p-5 rounded-md shadow-md flex gap-5">
+      <Link to={user.id.toString()} className='w-full'>
+      <p>Name: <span>{user.name}</span></p>
+      <p>Email: <span>{user.email}</span></p> 
+      </Link>
+      <div className="flex gap-5">
+        <button className="px-5  rounded-md bg-red-500 text-white">Delete</button>
+        <button className="px-5  rounded-md bg-teal-700 text-white">Update</button>
+      </div>     
+    </div>
+  ))
   return (
-    <h1 className="text-xl">
-      Welcome
-    </h1>
+    <>
+    <h1 className="font-bold text-xl">All Users</h1>
+    <div className='flex flex-col gap-1 pr-20'>
+    {users}
+    </div>
+    </>
   )
 }
---
+++ client/src/userData.ts
@@ -0,0 +1,36 @@
+import { User } from './types'
+const usersData: User[]= [
+  {
+    id:1,
+    name: "user1",
+    age: 21,
+    email: "user1@gmail.com",
+    profilePicture: "user1.png",
+    cv: "user1_cv.pdf",
+  },
+  {
+    id:2,
+    name: "user2",
+    age: 22,
+    email: "user2@gmail.com",
+    profilePicture: "user2.png",
+    cv: "user2_cv.pdf",
+  },
+  {
+    id:3,
+    name: "user3",
+    age: 23,
+    email: "user3@gmail.com",
+    profilePicture: "user3.png",
+    cv: "user3_cv.pdf",
+  },
+  {
+    id:4,
+    name: "user4",
+    age: 24,
+    email: "user4@gmail.com",
+    profilePicture: "user4.png",
+    cv: "user4_cv.pdf",
+  },
+];
+export {usersData}
