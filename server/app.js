a13d5487f3700823bdb9442bc5a944c1ffa90eb8
InshutiSouvede <isouvede2@gmail.com>
2024-08-27 22:42:48 +0200

    Add an endpoint for all users

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const userRouter = require('./routes/userRoutes')
 const app = express();
 const port = 4500
+app.get('/',(req,res)=>{
+    res.send("Welcome to root")
+})
+app.use('/users',userRouter)
 app.listen(port, (err) => {
   if (!err) console.log("Connected to ", port);
 });
