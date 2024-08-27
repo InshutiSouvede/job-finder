0285982688c5bba5269b4eab87da1dcf562d047b
InshutiSouvede <isouvede2@gmail.com>
2024-08-27 19:25:43 +0200

    chore:creat an app layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import AppLayout from './components/layouts/AppLayout.tsx'
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<AppLayout/>} >
+    <Route index element={<App/>} />
+  </Route>
+))
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
