3271ace4917745325ffdc9da51bfb6104dfaeb3e
InshutiSouvede <isouvede2@gmail.com>
2024-09-03 19:55:46 +0200

    Move server to src and add environment variables to test deployment

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { getAllUsers, getOneUser, deletUser } = require('../controllers/userController')
-const router = express.Router()
-router.get("/", getAllUsers)
-router.get("/:id", getOneUser)
-router.delete("/:id", deletUser)
-
-module.exports = router
