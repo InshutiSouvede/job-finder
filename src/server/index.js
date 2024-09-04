f03d3c31ec3b72a17361d6c8c6094cda98352117
InshutiSouvede <isouvede2@gmail.com>
2024-09-04 10:15:11 +0200

    Add more environment variables

--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const cors = require('cors')
 const userRouter = require('./routes/userRoutes');
-
-const { USER_NAME, PASSWORD } = require("./credential");
+dotenv.config()
+const USER_NAME = process.env.USER_NAME
+const PASSWORD = process.env.PASSWORD
 const connect = mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@alldbs.plbwm.mongodb.net/job_finder_app?retryWrites=true&w=majority&appName=allDbs`)
 connect.then((data)=>console.log("Successfully connected to the database"))
 .catch((err)=> console.log("Could not connect to the database",err))
 
 const app = express();
 // const port = 4500
-dotenv.config()
+
 const port = process.env.PORT
 app.use(cors())
 app.get('/',(req,res)=>{
