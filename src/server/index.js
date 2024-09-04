82c443a039d0db0c9bd6cec262d2b2433c18f342
InshutiSouvede <isouvede2@gmail.com>
2024-09-04 15:29:02 +0200

    Remove the connection options

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,7 @@ const userRouter = require('./routes/userRoutes');
 dotenv.config()
 const USER_NAME = process.env.USER_NAME
 const PASSWORD = process.env.PASSWORD
-const connect = mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@alldbs.plbwm.mongodb.net/job_finder_app?retryWrites=true&w=majority&appName=allDbs`,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+const connect = mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@alldbs.plbwm.mongodb.net/job_finder_app?retryWrites=true&w=majority&appName=allDbs`)
 connect.then((data)=>console.log("Successfully connected to the database"))
 .catch((err)=> console.log("Could not connect to the database",err))
 
